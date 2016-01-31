2e619209abc0f3554379a8ea38d867c5c465d420
flvckx <eqwem@ukr.net>
2016-01-31 14:39:39 +0200

    task 1

---
+++ task 1/Task 1/Task 1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task 1
+//
+//  Created by Anatoliy on 1/31/16.
+//  Copyright © 2016 mobex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
